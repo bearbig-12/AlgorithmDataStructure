9600574fb556040559f7b45a3ffa7d4d22c4d5dd
bearbig-12 <bearbig23@gmail.com>
2025-07-15 14:01:29 +0900

    링크리스트 스택 업데이트

---
+++ DataStructure/LinkedListStack/LinkedListStack_main.c
@@ -0,0 +1,49 @@
+﻿#include "LinkedListStack.h"
+
+
+int main()
+{
+    int i = 0;
+    int Count = 0;
+    Node* Popped;
+
+    LinkedListStack* Stack;
+
+    CreateStack(&Stack);
+
+    Push(Stack, CreateNode("abc"));
+    Push(Stack, CreateNode("def"));
+    Push(Stack, CreateNode("efg"));
+    Push(Stack, CreateNode("hij"));
+    Push(Stack, CreateNode("klm"));
+
+
+    Count = GetSize(Stack);
+    printf("Size: %d, Top: %s\n\n",
+        Count, FindTop(Stack)->mData);
+
+    for (i = 0; i < Count; i++)
+    {
+        if (IsEmpty(Stack))
+            break;
+
+        Popped = Pop(Stack);
+
+        printf("Popped: %s, ", Popped->mData);
+
+        DestroyNode(Popped);
+
+        if (!IsEmpty(Stack))
+        {
+            printf("Current Top: %s\n", FindTop(Stack)->mData);
+        }
+        else
+        {
+            printf("Stack Is Empty.\n");
+        }
+    }
+
+    DestroyStack(Stack);
+
+    return 0;
+}
