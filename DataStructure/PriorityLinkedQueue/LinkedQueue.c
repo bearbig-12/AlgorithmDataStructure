a5986651d12449ebcf0c026c942a8ff837be40da
Hyeontae An <bearbig23@gmail.com>
2025-08-08 13:54:11 +0900

    우선순위 큐(LinkedList) 업데이트

--- DataStructure/PriorityLinkedQueue/LinkedQueue.c
+++ DataStructure/PriorityLinkedQueue/LinkedQueue.c
@@ -21,13 +21,14 @@ void DestroyQueue(LinkedQueue* Queue)
 	free(Queue);
 }
 
-Node* CreateNode(char* NewData)
+Node* CreateNode(int Priority, char* NewData)
 {
 	Node* newNode = (Node*)malloc(sizeof(Node));
 	newNode->mData = (char*)malloc(strlen(NewData) + 1);
 
 	strcpy(newNode->mData, NewData);  //  데이터를 저장한다. 
 
+	newNode->priority = Priority;
 	newNode->mNextNode = NULL; //  다음 노드에 대한 포인터는 NULL로 초기화한다. 
 
 	return newNode;//  노드의 주소를 반환한다. 
@@ -46,6 +47,9 @@ void Enqueue(LinkedQueue* Queue, Node* NewNode)
 		Queue->mFront = NewNode;
 		Queue->mRear = NewNode;
 		Queue->mCount++;
+	}
+	if (NewNode->priority <= Queue->mFront->priority && Queue->mFront->mNextNode != NULL)
+	{
 	}
 	else
 	{
--- DataStructure/PriorityLinkedQueue/LinkedQueue_Main.c
+++ DataStructure/PriorityLinkedQueue/LinkedQueue_Main.c
@@ -1,4 +1,6 @@
-﻿#include "LinkedQueue.h"
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+#include "LinkedQueue.h"
 
 int main(void)
 {
@@ -7,11 +9,32 @@ int main(void)
 
     CreateQueue(&Queue);
 
-    Enqueue(Queue, CreateNode("abc"));
-    Enqueue(Queue, CreateNode("def"));
-    Enqueue(Queue, CreateNode("efg"));
-    Enqueue(Queue, CreateNode("hij"));
+    char Data[100]; // 문자열 입력 공간
+    int priority = 0;
+    while (1)
+    {
+        printf("우선 순위 큐에 저장할 데이터를 입력하세요 : ");
+        scanf("%d, %s", &priority, Data);
+        
+        if (priority <= -1) // 입력 중단
+        {
+            break;
+        }
+        // 입력받은 데이터를 큐에 저장한다.
+        Enqueue(Queue, CreateNode(priority, Data));
 
+        // 큐에 입력된 데이터를 출력
+        Node* Current = Queue->mFront;  //헤드 노드의 주소값을 current에 저장
+
+        while (Current != NULL)
+        {
+            printf("[Priority : %d, Data : %s\n]", Current->priority, Current->mData);
+
+            Current = Current->mNextNode;
+        }
+    }
+
+   
     printf("Queue Size : %d\n", Queue->mCount);
 
     while (IsEmpty(Queue) == 0)
