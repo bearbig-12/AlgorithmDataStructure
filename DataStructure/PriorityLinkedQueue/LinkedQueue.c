de567036f79c7d653973902fd7879902936f8ff7
Hyeontae An <bearbig23@gmail.com>
2025-08-08 13:51:59 +0900

    우선순위 큐(Linked List) 업데이트

---
+++ DataStructure/PriorityLinkedQueue/LinkedQueue.c
@@ -0,0 +1,81 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+
+#include "LinkedQueue.h"
+
+void CreateQueue(LinkedQueue** Queue)
+{
+	(*Queue) = (LinkedQueue*)malloc(sizeof(LinkedQueue));
+	(*Queue)->mFront = NULL;
+	(*Queue)->mRear = NULL;
+	(*Queue)->mCount = 0;
+}
+
+void DestroyQueue(LinkedQueue* Queue)
+{
+	while (!IsEmpty(Queue))
+	{
+		Node* Popped = Dequeue(Queue);
+		DestroyNode(Popped);
+	}
+	free(Queue);
+}
+
+Node* CreateNode(char* NewData)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->mData = (char*)malloc(strlen(NewData) + 1);
+
+	strcpy(newNode->mData, NewData);  //  데이터를 저장한다. 
+
+	newNode->mNextNode = NULL; //  다음 노드에 대한 포인터는 NULL로 초기화한다. 
+
+	return newNode;//  노드의 주소를 반환한다. 
+}
+
+void  DestroyNode(Node* _Node)
+{
+	free(_Node->mData);
+	free(_Node);
+}
+
+void Enqueue(LinkedQueue* Queue, Node* NewNode)
+{
+	if (Queue->mFront == NULL)
+	{
+		Queue->mFront = NewNode;
+		Queue->mRear = NewNode;
+		Queue->mCount++;
+	}
+	else
+	{
+		Queue->mRear->mNextNode = NewNode;
+		Queue->mRear = NewNode;
+		Queue->mCount++;
+	}
+}
+
+Node* Dequeue(LinkedQueue* Queue)
+{
+	//  LQ_Dequeue() 함수가 반환할 최상위 노드 
+	Node* Front = Queue->mFront;
+
+	if (Queue->mFront->mNextNode == NULL)
+	{
+		Queue->mFront = NULL;
+		Queue->mRear = NULL;
+	}
+	else
+	{
+		Queue->mFront = Queue->mFront->mNextNode;
+	}
+
+	Queue->mCount--;
+
+	return Front;
+}
+
+int IsEmpty(LinkedQueue* Queue)
+{
+	return (Queue->mFront == NULL);
+}
---
+++ DataStructure/PriorityLinkedQueue/LinkedQueue.h
@@ -0,0 +1,34 @@
+﻿#ifndef LINKED_QUEUE_H
+#define LINKED_QUEUE_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct tagNode
+{
+	int priority; // 우선 순위
+	char* mData;
+	struct tagNode* mNextNode;
+
+}Node;
+
+typedef struct tagLinkedQueue
+{
+	Node* mFront;
+	Node* mRear;
+	int mCount;
+
+}LinkedQueue;
+
+void  CreateQueue(LinkedQueue** Queue);
+void  DestroyQueue(LinkedQueue* Queue);
+
+Node* CreateNode(int Priority , char* Data);
+void  DestroyNode(Node* _Node);
+
+void  Enqueue(LinkedQueue* Queue, Node* NewNode);
+Node* Dequeue(LinkedQueue* Queue);
+
+int   IsEmpty(LinkedQueue* Queue);
+
+#endif
